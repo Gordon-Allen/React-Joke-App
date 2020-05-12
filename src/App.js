d7d56df3fa1a6eedc8b6b2ccb19df74885a1fd5f
Gordon-Allen <gordonmichaelallen@gmail.com>
2020-05-12 16:10:08 -0400

    Initial code-clean up review

--- src/App.js
+++ src/App.js
@@ -4,42 +4,42 @@ import ReactDOM from "react-dom"
 import Joke from './Joke';
 import jokesData from './jokesdata';
 
-// class App extends React.Component {
-//   jokeComponents = jokesData.map((joke) => (
-//     <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
-//   ));
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1><em>Looking For a Laugh? </em>Here's A Few Office-Friendly Knee-Slappers For Ya:</h1>
-//           {this.jokeComponents}
-//       </div>
-//     );
-//   }}
+class App extends React.Component {
+  jokeComponents = jokesData.map((joke) => (
+    <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
+  ));
+  render() {
+    return (
+      <div className="App">
+        <h1><em>Looking For a Laugh? </em>Here's A Few Office-Friendly Knee-Slappers For Ya:</h1>
+          {this.jokeComponents}
+      </div>
+    );
+  }}
+
+  export default App;
   
   // const jokeComponents = jokesData.map((joke) => (
   //   <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
   // ));
 
-export class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {isLoggedIn: true};
-    this.toggleLogin = this.toggleLogin.bind(this);
-  }
-  toggleLogin() {
-    this.setState(() => {return {isLoggedIn: true}})
-  }
+// export class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {isLoggedIn: true};
+//     this.toggleLogin = this.toggleLogin.bind(this);
+//   }
+//   toggleLogin() {
+//     this.setState(() => {return {isLoggedIn: true}})
+//   }
 
-  render = () => {
-    return (
-      <div>
-        You are currently logged {this.state.isLoggedIn ? "in" : "out"}
-        <button onClick={this.toggleLogin}>Switch</button>
-      </div>
-    );
-  };
+//   render = () => {
+//     return (
+//       <div>
+//         You are currently logged {this.state.isLoggedIn ? "in" : "out"}
+//         <button onClick={this.toggleLogin}>Switch</button>
+//       </div>
+//     );
+//   };
 
-}
-
-export default App;
+// }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import Joke from './Joke';
+import jokesData from './jokesdata';
+
+class App extends React.Component {
+  jokeComponents = jokesData.map((joke) => (
+    <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
+  ));
+  render() {
+    return (
+      <div className="App">
+        <h1><em>Looking For a Laugh? </em>Here's A Few Office-Friendly Knee-Slappers For Ya:</h1>
+          {this.jokeComponents}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
 
 // #1
 // function App() {
@@ -52,49 +70,26 @@ import ReactDOM from "react-dom"
 //     }
 // }
 
-// class App extends React.Component {
-//   jokeComponents = jokesData.map((joke) => (
-//     <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
-//   ));
-//   render() {
+// export class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {isLoggedIn: true};
+//     this.toggleLogin = this.toggleLogin.bind(this);
+//   }
+//   toggleLogin() {
+//     this.setState(() => {return {isLoggedIn: !this.state.isLoggedIn}})
+//   }
+
+//   render = () => {
 //     return (
-//       <div className="App">
-//         <h1><em>Looking For a Laugh? </em>Here's A Few Office-Friendly Knee-Slappers For Ya:</h1>
-//           {this.jokeComponents}
+//       <div>
+//         You are currently logged {this.state.isLoggedIn ? "in" : "out"}
+//         <button onClick={this.toggleLogin}>Switch</button>
 //       </div>
 //     );
-//   }}
-  
-  // const jokeComponents = jokesData.map((joke) => (
-  //   <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
-  // ));
+//   }
 
-export class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {isLoggedIn: true};
-    this.toggleLogin = this.toggleLogin.bind(this);
-  }
-  toggleLogin() {
-    this.setState(() => {return {isLoggedIn: !this.state.isLoggedIn}})
-  }
-
-  render = () => {
-    return (
-      <div>
-        You are currently logged {this.state.isLoggedIn ? "in" : "out"}
-        <button onClick={this.toggleLogin}>Switch</button>
-      </div>
-    );
-  }
-
-}
-
-export default App;
-
-
-
-
+// }
 
 // class Login extends React.Component {
 //     state = {
@@ -117,11 +112,3 @@ export default App;
 //     };
 
 // };
-
-
-
-
-
-
-
-ReactDOM.render(<App />, document.getElementById("root"))
